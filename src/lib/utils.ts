3b426279e004f834a753adfed0cb28005db346ce
rezcoco <rezaainulzaki23@gmail.com>
2024-07-31 20:03:11 +0700

    fix error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import conjugator from "./korean/conjugator"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
